09f6acf35c73039a4af719cb3926cbaadfc6c2bb
GUMMINJAE (琴旼賳) <globaljavateacher@gmail.com>
2026-08-04 16:34:42 +0900

    Add files via upload

---
+++ src/main/java/com/global/itct/service/BankService.java
@@ -0,0 +1,11 @@
+package com.global.itct.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankService {
+	
+	public boolean isServerActive() {
+		return false;
+	}
+}
